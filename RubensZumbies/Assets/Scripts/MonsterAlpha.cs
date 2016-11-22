ac0241ff9d9a5b365915a81a7d04e20392d05d6b
Rubensaccnt <santipab.stephane@gmail.com>
2016-11-22 15:17:50 -0600

    Game project changes: Added gameplayer object to main test scene, grouped static objects together, reorganized prefabs, recreated some prefabs, and enabled disabled prefabs.
    Code changes:
    - Some of the explosions stopped working, so I fixed that.
    - Monsters not moving at specified speed, fixed that
    - Monsters requiring monster prefabs to start the spawn chain, monsters can now be spawned without initial prefabs (see monster manager)
    
    Note on the disabled prefabs: Due to the project missing a key component, the unity build attempted to upgrade and failed, thus previous prefabs got disabled.
    
    Future changes: 3d models of monsters with basic animations, and nav mesh AI coded to navigate non-planar terrain.

---
+++ RubensZumbies/Assets/Scripts/MonsterAlpha.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAlpha : MonoBehaviour {
+
+    GameObject player;
+
+    [SerializeField]
+    string playerName;
+
+    [SerializeField]
+    float moveSpeed = 1;
+
+    [SerializeField]
+    public float initialHealth = 10;
+
+    public float health;
+    [SerializeField]
+    string gameManagerName= "MonsterGameManager";
+
+    [SerializeField]
+    GameObject destroyEffect;
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+            CheckHealth();
+        }
+    }
+
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find(playerName);
+        health = initialHealth;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+
+        Vector3 lookRot = (player.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(lookRot);
+	}
+    
+    void CheckHealth()
+    {
+        if (health <= 0)
+        {
+            KillMonster();
+        }
+    }
+
+    public void KillMonster()
+    {
+        GameObject.Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        SpawnMonster();
+    }
+
+    void SpawnMonster()
+    {
+        // call game manager for spawner event
+        GameObject.Find(gameManagerName).GetComponent<MonsterGameManager>().Spawn();
+    }
+
+}
